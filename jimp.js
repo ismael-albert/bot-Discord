2aaf1744ea1bed372bcdb5fe633fe31dcbacac36
ismael-albert <62386498+ismael-albert@users.noreply.github.com>
2021-05-26 21:17:04 -0300

    Update jimp.js

--- jimp.js
+++ jimp.js
@@ -7,7 +7,7 @@ let mask = await jimp.read("mascara.png")
 let fundo = await jimp.read("fundo.png")
 
 
-jimp.read('https://sm.ign.com/ign_br/news/a/avatar-the/avatar-the-last-airbender-is-getting-expansion-novels_sma8.jpg').then(avatar => {
+jimp.read('').then(avatar => {
     // Do stuff with the image.
 
 avatar.resize(130, 130)
